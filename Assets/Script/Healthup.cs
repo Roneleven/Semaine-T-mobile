83101d44b7fe4488af492879ff2ae68f169d7223
Martin <martinmagicdragon@gmail.com>
2023-11-23 14:22:30 +0100

    encore

--- Assets/Script/Healthup.cs
+++ Assets/Script/Healthup.cs
@@ -13,7 +13,7 @@ public class Healthup : MonoBehaviour
         {
             Health healthScript = other.GetComponent<Health>();
             // Increase health by 2 using the Health script reference
-            healthScript.health += 2;
+            healthScript.maxHealth += 2;
 
             // Optional: Add any other logic or effects you want when health is increased , mettre ui a jour
 
