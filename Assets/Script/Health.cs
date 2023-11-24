0ec1a59db6b5e13cc515fcbdb55ef44ee6acb894
Wassim Boulatika <wassim.boulatika@gmail.com>
2023-11-24 12:36:14 +0100

    added blink to player when hit

--- Assets/Script/Health.cs
+++ Assets/Script/Health.cs
@@ -12,11 +12,25 @@ public class Health : MonoBehaviour
     public float damageAmount = 1f; // Variable publique pour définir la valeur de dégâts
     public HealthBarUpdater healthBarUpdater; // Référence à HealthBarUpdater
     public GameObject gameOverUI; // Référence à votre UI Game Over
+    public Renderer playerRenderer; // Référence au Renderer du joueur (assurez-vous d'ajouter un Renderer au joueur)
+
+    private Color originalColor;
+    private Color immuneColor = new Color(1f, 0f, 0f, 0.5f); // Rouge transparent
 
     // Start is called before the first frame update
     void Start()
     {
         currentHealth = maxHealth;
+
+        // Assurez-vous qu'il y a un Renderer attaché au joueur
+        if (playerRenderer != null)
+        {
+            originalColor = playerRenderer.material.color;
+        }
+        else
+        {
+            Debug.LogError("Veuillez attacher un Renderer au joueur.");
+        }
     }
 
     // Update is called once per frame
@@ -31,8 +45,23 @@ public class Health : MonoBehaviour
             if (immuneTimer <= 0f)
             {
                 isImmune = false;
+                // Rétablir la couleur originale lorsque l'immunité est terminée
+                playerRenderer.material.color = originalColor;
+            }
+            else
+            {
+                // Faire clignoter en rouge
+                StartCoroutine(Blink());
             }
         }
+    }
+
+    IEnumerator Blink()
+    {
+        playerRenderer.material.color = immuneColor;
+        yield return new WaitForSeconds(0.1f); // Temps pendant lequel la couleur rouge est affichée
+        playerRenderer.material.color = originalColor;
+        yield return new WaitForSeconds(0.1f); // Temps pendant lequel la couleur originale est affichée
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -60,6 +89,8 @@ public class Health : MonoBehaviour
                 if (gameOverUI != null)
                 {
                     gameOverUI.SetActive(true);
+
+                    // Mettre en pause le jeu
                     Time.timeScale = 0f;
                 }
             }
